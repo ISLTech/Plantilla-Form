84242f92b5759adcaa4729586afaf147f2465ff0
SVLucasAndres <lucassangurima2@gmail.com>
2024-09-12 14:52:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Plantilla-Form',
+  webDir: 'www'
+};
+
+export default config;
